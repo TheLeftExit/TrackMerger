bbd7e3f9af4e8e858a41dda7642e59e8fc7f5c4a
TheLeftExit <theleftexit@protonmail.com>
2022-06-11 00:28:26 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿const string WORKDIR = @"C:\Users\TheLeftExit\Desktop\Color Theory\Patreon Exclusives";
+const string FFMPEG = @"C:\ffmpeg\ffmpeg.exe";
+
+Directory.SetCurrentDirectory(WORKDIR);
+foreach(string mp3Name in Directory.GetFiles(".", "*.mp3")) {
+    var wavName = Path.ChangeExtension(mp3Name, ".wav");
+    var flacName = Path.ChangeExtension(mp3Name, ".flac");
+    var txtName = Path.ChangeExtension(mp3Name, ".txt");
+    if (!File.Exists(wavName)) throw new ApplicationException($"Could not match {mp3Name} to a WAV file.");
+    
+    //var cmdArgs = $"-i \"{mp3Name}\" -i \"{wavName}\" \"{flacName}\"";
+    var cmdArgs1 = $"-i \"{mp3Name}\" -f ffmetadata \"{txtName}\"";
+    var cmdArgs2 = $"-f ffmetadata -i \"{txtName}\" -i \"{wavName}\" \"{flacName}\"";
+    await System.Diagnostics.Process.Start(FFMPEG, cmdArgs1).WaitForExitAsync();
+    await System.Diagnostics.Process.Start(FFMPEG, cmdArgs2).WaitForExitAsync();
+    File.Delete(txtName);
+}
